2caee15c755e49d4a0e32d9347765c4d732aeb8d
kbrother102 <kbrother102@gmail.com>
2026-04-01 21:28:30 +0900

    [김형백] 싹날림

--- Source/Vigilant/Interaction/VGInteractable.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interaction/VGInteractable.h"
-
-// Add default functionality here for any IVGInteractable functions that are not pure virtual.
