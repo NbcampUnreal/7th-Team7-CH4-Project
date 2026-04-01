e5e66edad891cb50e75cd1f6be158c93a98633de
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2026-04-01 09:33:30 +0900

    [장유진] 기본 클래스 생성

---
+++ Source/Vigilant/Character/VGAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "VGAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIGILANT_API UVGAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
