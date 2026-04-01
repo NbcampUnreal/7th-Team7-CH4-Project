af3b75fff0361b073557f6cd7f42ae6b4bb83176
kbrother102 <kbrother102@gmail.com>
2026-04-01 16:50:19 +0900

    [김형백] 게임플레이 태그 캐싱 클래스 제작

---
+++ Source/Vigilant/Common/VGGameplayTags.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VGGameplayTags.h"
+
+//지금 아래건 예시 입니다!, 따옴표 안의 글자 오타 조심!
+UE_DEFINE_GAMEPLAY_TAG(VigilantTags::Sprint, "Character.Action.Movable.Sprint");
+
+
+
+VGGameplayTags::VGGameplayTags()
+{
+}
+
+VGGameplayTags::~VGGameplayTags()
+{
+}
