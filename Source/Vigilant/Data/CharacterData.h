fe5d0a5a1b123fe88ce257c481fea4b101ee39d1
kbrother102 <kbrother102@gmail.com>
2026-04-01 17:45:59 +0900

    [김형백] 터지기전 작업 데이터

---
+++ Source/Vigilant/Data/CharacterData.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "CharacterData.generated.h"
+/**
+ * 
+ */
+USTRUCT()
+struct FCharacterData : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	//변수선언
+	
+	
+};
