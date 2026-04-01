c54e225084cb70b574c2b73edfad9b9f24f1bc29
tkd6966 <tkd6966@gmail.com>
2026-04-01 17:53:01 +0900

    [하상빈] VGBossCharacter 광역 공격 RPC 구현
    - Server_PerformAttack: 서버 전용 광역 데미지(ApplyRadialDamage) 판정 구현
    - NetMulticast_PlayAttackEffects: 클라이언트 동기화용 이펙트(DrawDebugSphere) 및 몽타주 재생 구현
    - Editor: DA_BossMain 데이터 에셋 생성 및 스탯/몽타주 할당
    - Editor: BP_BossCharacter에 BossData 연동 및 BeginPlay 3초 지연 자동 공격 테스트 로직 추가

--- Source/Vigilant/Character/VGBossCharacter.cpp
+++ Source/Vigilant/Character/VGBossCharacter.cpp
@@ -6,6 +6,8 @@
 #include "Data/VGBossDataAsset.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Net/UnrealNetwork.h"
+#include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
 
 AVGBossCharacter::AVGBossCharacter()
 {
@@ -28,6 +30,47 @@ void AVGBossCharacter::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty
 	
 	DOREPLIFETIME(AVGBossCharacter, CurrentHealth);
 	DOREPLIFETIME(AVGBossCharacter, AttackDamage);
+}
+
+void AVGBossCharacter::Server_PerformAttack_Implementation()
+{
+	// 데미지 계산은 서버에서만 진행
+	if (HasAuthority())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("서버: 보스 광역 공격 실행"));
+
+		// 광역 데미지 적용을 위한 무시 목록 (자기 자신은 데미지를 안 받음)
+		TArray<AActor*> IgnoreActors;
+		IgnoreActors.Add(this);
+
+		// 언리얼 내장 함수를 활용한 구형(Radial) 데미지 적용
+		UGameplayStatics::ApplyRadialDamage(
+			this,                  // WorldContextObject
+			AttackDamage,          // 데미지 량 (데이터 에셋에서 가져온 값)
+			GetActorLocation(),    // 공격 중심점 (보스 위치)
+			AttackRadius,          // 공격 반경 (데이터 에셋에서 가져온 값)
+			nullptr,               // 데미지 타입 클래스 (기본값)
+			IgnoreActors,          // 무시할 액터 목록
+			this,                  // 데미지 유발자 (보스)
+			GetController(),       // 공격자 컨트롤러
+			true                   // 장애물을 관통해서 데미지를 줄 것인지 여부
+		);
+
+		// 데미지 처리가 끝났으니, 모든 플레이어의 화면에 공격 이펙트를 재생하라고 지시
+		NetMulticast_PlayAttackEffects();
+	}
+}
+
+void AVGBossCharacter::NetMulticast_PlayAttackEffects_Implementation()
+{
+	// 시각적 디버깅
+	DrawDebugSphere(GetWorld(), GetActorLocation(), AttackRadius, 32, FColor::Red, false, 2.0f);
+
+	// 데이터 에셋에서 할당받은 몽타주가 있다면 애니메이션 재생
+	if (AttackMontage != nullptr)
+	{
+		PlayAnimMontage(AttackMontage);
+	}
 }
 
 void AVGBossCharacter::BeginPlay()
