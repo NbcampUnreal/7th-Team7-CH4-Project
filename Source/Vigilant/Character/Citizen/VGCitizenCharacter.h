275a385ecb2f1b868f98f3227b4fe57fbff048d3
kbrother102 <kbrother102@gmail.com>
2026-04-01 17:40:55 +0900

    [김형백] 게임플레이태그 수정 인터페이스 함수 캐릭터에 구현

--- Source/Vigilant/Character/Citizen/VGCitizenCharacter.h
+++ Source/Vigilant/Character/Citizen/VGCitizenCharacter.h
@@ -3,15 +3,32 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagAssetInterface.h"
 #include "Character/VGCharacterBase.h"
+#include "Character/CharacterInterface/VGCharacterGameplayTagEditor.h"
 #include "VGCitizenCharacter.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class VIGILANT_API AVGCitizenCharacter : public AVGCharacterBase
+class VIGILANT_API AVGCitizenCharacter : 
+public AVGCharacterBase, 
+public IVGCharacterGameplayTagEditor,
+public IGameplayTagAssetInterface
+
 {
 	GENERATED_BODY()
 	
+	
+#pragma region Interfaces Func
+public:
+	virtual void GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const override;
+	virtual void AddGameplayTag(FGameplayTag TagToAdd) override;
+	virtual void RemoveGameplayTag(FGameplayTag TagToRemove) override;
+#pragma endregion 김형백
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character|Tags")
+	FGameplayTagContainer CharacterGameplayTags;
 };
