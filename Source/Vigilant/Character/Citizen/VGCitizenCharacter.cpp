154cb569d959e7613d501595e5ff74c0c428b521
kbrother102 <kbrother102@gmail.com>
2026-04-01 17:17:02 +0900

    [김형백] 읽기전용 해제

--- Source/Vigilant/Character/Citizen/VGCitizenCharacter.cpp
+++ Source/Vigilant/Character/Citizen/VGCitizenCharacter.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Character/VGCitizenCharacter.h"
+#include "Character/Citizen/VGCitizenCharacter.h"
 
--- Source/Vigilant/Common/VGGameplayTags.cpp
+++ Source/Vigilant/Common/VGGameplayTags.cpp
@@ -4,7 +4,7 @@
 #include "VGGameplayTags.h"
 
 //지금 아래건 예시 입니다!, 따옴표 안의 글자 오타 조심!
-UE_DEFINE_GAMEPLAY_TAG(VigilantTags::Sprint, "Character.Action.Movable.Sprint");
+//UE_DEFINE_GAMEPLAY_TAG(VigilantTags::Sprint, "Character.Action.Movable.Sprint");
 
 
 
