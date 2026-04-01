827e9c0f7ceac3fad933656ca92401c9af80e92f
kbrother102 <kbrother102@gmail.com>
2026-04-01 15:09:19 +0900

    [김형백] 초기 데이터 Make

---
+++ Source/Vigilant/Data/WeaponDataAsset.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VGEquipmentDataAsset.h"
+#include "WeaponDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class VIGILANT_API UWeaponDataAsset : public UVGEquipmentDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+	float Damage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+	float AttackSpeed;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+	UAnimMontage* AttackMontage;
+    
+	// 방어/패링 시스템을 위한 몽타주 추가
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon")
+	UAnimMontage* BlockMontage;
+};
