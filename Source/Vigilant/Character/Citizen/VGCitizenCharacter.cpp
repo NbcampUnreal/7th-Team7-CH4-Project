a828fb5e604fa52dea2e8c168d097cee9e21d0c3
kbrother102 <kbrother102@gmail.com>
2026-04-01 17:40:55 +0900

    [김형백] 게임플레이태그 수정 인터페이스 함수 캐릭터에 구현

--- Source/Vigilant/Character/Citizen/VGCitizenCharacter.cpp
+++ Source/Vigilant/Character/Citizen/VGCitizenCharacter.cpp
@@ -3,3 +3,17 @@
 
 #include "Character/Citizen/VGCitizenCharacter.h"
 
+void AVGCitizenCharacter::GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const
+{
+	TagContainer = CharacterGameplayTags
+}
+
+void AVGCitizenCharacter::AddGameplayTag(FGameplayTag TagToAdd)
+{
+	CharacterGameplayTags.AddTag(TagToAdd);
+}
+
+void AVGCitizenCharacter::RemoveGameplayTag(FGameplayTag TagToRemove)
+{
+	CharacterGameplayTags.RemoveTag(TagToRemove);
+}
