e02258a154c1751270d0457ece406ad566338af5
h-h4 <231655381+h-h4@users.noreply.github.com>
2026-04-01 17:59:11 +0900

    [이현진] 빌드오류 수정
    중복 정의 오류를 수정하였습니다

--- Source/Vigilant/Interaction/VGInteractable.cpp
+++ Source/Vigilant/Interaction/VGInteractable.cpp
@@ -3,14 +3,4 @@
 
 #include "Interaction/VGInteractable.h"
 
-// Add default functionality here for any IVGInteractable functions that are not pure virtual.
 
-bool IVGInteractable::CanInteract_Implementation(AVGCharacterBase* Interactor) const
-{
-	return false; 
-}
-
-void IVGInteractable::OnInteract_Implementation(AVGCharacterBase* Interactor)
-{
-	
-}
