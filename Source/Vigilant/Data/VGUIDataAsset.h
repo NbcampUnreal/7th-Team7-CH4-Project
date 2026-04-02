729ea750354a19c956d55a5b1748a0db588f7f0e
kbrother102 <kbrother102@gmail.com>
2026-04-02 17:11:28 +0900

    [김형백] UI 클래스 데이터 에셋 추가

---
+++ Source/Vigilant/Data/VGUIDataAsset.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "VGUIDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIGILANT_API UVGUIDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "HUD")
+	TSubclassOf<UUserWidget> MainHUDWidgetClass;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "HUD")
+	TSubclassOf<UUserWidget> PopupWidgetClass;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "HUD")
+	TSubclassOf<UUserWidget> VoteWidgetClass;
+};
