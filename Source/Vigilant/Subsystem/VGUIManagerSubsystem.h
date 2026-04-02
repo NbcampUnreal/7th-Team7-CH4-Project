1b87e15986fcedeb27c5cf51cbc1326bace312a8
kbrother102 <kbrother102@gmail.com>
2026-04-02 14:03:31 +0900

    [김형백] UI 서브시스템과 위젯 클래스 제작

---
+++ Source/Vigilant/Subsystem/VGUIManagerSubsystem.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/LocalPlayerSubsystem.h"
+#include "VGUIManagerSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIGILANT_API UVGUIManagerSubsystem : public ULocalPlayerSubsystem
+{
+	GENERATED_BODY()
+};
---
+++ Source/Vigilant/UI/VGHUDWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputBlueprintNodes/Private/EnhancedInputUserWidgetValidator.h"
+#include "VGHUDWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIGILANT_API UVGHUDWidget : public UEnhancedInputUserWidgetValidator
+{
+	GENERATED_BODY()
+};
