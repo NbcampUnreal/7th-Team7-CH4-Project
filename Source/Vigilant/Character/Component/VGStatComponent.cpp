ae47296578ed0c69b2c65442fdfd779152f980b7
h-h4 <231655381+h-h4@users.noreply.github.com>
2026-04-02 10:38:25 +0900

    [이현진] 기초 생성

---
+++ Source/Vigilant/Character/Component/VGStatComponent.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VGStatComponent.h"
+
+
+UVGStatComponent::UVGStatComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+
+void UVGStatComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	
+}
+
+
+void UVGStatComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                     FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+}
+
---
+++ Source/Vigilant/Character/Component/VGStatComponent.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "VGStatComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class VIGILANT_API UVGStatComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UVGStatComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
