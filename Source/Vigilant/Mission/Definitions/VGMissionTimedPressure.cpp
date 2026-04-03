88d9a7e7f08d3b6d3b78275e2ebcd304e2b4a611
정찬호_JCH <elvpfhhrm@gmail.com>
2026-04-03 15:56:31 +0900

    [정찬호] Mission 추가 구현
    다음 미션의 클래스를 작성했습니다.
    지정한 레벌들 중 특정 레버들만 올리기
    제한 시간 내에 모든 발판 밟기

---
+++ Source/Vigilant/Mission/Definitions/VGMissionTimedPressure.cpp
@@ -0,0 +1,64 @@
+﻿#include "VGMissionTimedPressure.h"
+#include "Common/VGGameplayTags.h"
+#include "Mission/Gimmick/VGMissionGimmickBase.h"
+
+AVGMissionTimedPressure::AVGMissionTimedPressure()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AVGMissionTimedPressure::OnGimmickStateChanged(AVGMissionGimmickBase* Gimmick, FGameplayTag Tag)
+{
+	if (Tag != VigilantMissionTags::GimmickActive)
+	{
+		return;
+	}
+	
+	if (CurrentStateTag == VigilantMissionTags::MissionInactive)
+	{
+		StartTimer();
+		SetMissionState(VigilantMissionTags::MissionActive);
+	}
+	
+	if (AreAllPressuresActive())
+	{
+		CompleteMission();
+	}
+}
+
+void AVGMissionTimedPressure::StartTimer()
+{
+	GetWorldTimerManager().SetTimer(
+		MissionTimerHandle,
+		this,
+		&AVGMissionTimedPressure::OnTimerExpired,
+		TimeLimit,  // MissionBase에 이미 있는 변수
+		false);
+}
+
+void AVGMissionTimedPressure::OnTimerExpired()
+{
+	SetMissionState(VigilantMissionTags::MissionInactive);
+	for (AVGMissionGimmickBase* Gimmick : MissionGimmicks)
+	{
+		if (Gimmick)
+		{
+			Gimmick->ResetGimmickState();
+		}
+	}
+}
+
+bool AVGMissionTimedPressure::AreAllPressuresActive() const
+{
+	for (AVGMissionGimmickBase* Gimmick : MissionGimmicks)
+	{
+		if (!Gimmick) continue;
+		if (Gimmick->GetStateTag() != VigilantMissionTags::GimmickActive &&
+			Gimmick->GetStateTag() != VigilantMissionTags::GimmickCompleted)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
---
+++ Source/Vigilant/Mission/Definitions/VGMissionTimedPressure.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "VGMissionBase.h"
+#include "VGMissionTimedPressure.generated.h"
+
+UCLASS()
+class VIGILANT_API AVGMissionTimedPressure : public AVGMissionBase
+{
+	GENERATED_BODY()
+
+public:
+	AVGMissionTimedPressure();
+
+protected:
+	virtual void OnGimmickStateChanged(
+			AVGMissionGimmickBase* Gimmick, FGameplayTag Tag) override;
+private:
+	void StartTimer();
+	void OnTimerExpired();
+	bool AreAllPressuresActive() const;
+};
