075b5af30d0ba7144a45361e97415923e306fc3c
정찬호_JCH <elvpfhhrm@gmail.com>
2026-04-03 21:54:00 +0900

    [정찬호] Mission 연동 테스트 용도의 Widget class 생성

---
+++ Source/Vigilant/UI/VGMissionInfoWidget.cpp
@@ -0,0 +1,33 @@
+﻿#include "VGMissionInfoWidget.h"
+#include "Mission/Definitions/VGMissionBase.h"
+#include "Components/TextBlock.h"
+#include "Common/VGGameplayTags.h"
+
+void UVGMissionInfoWidget::RegisterMission(AVGMissionBase* Mission)
+{
+	if (!Mission)
+	{
+		return;
+	}
+	
+	MissionDescriptionText->SetText(FText::FromString(Mission->GetMissionDecription()));
+	Mission->OnMissionStateChanged.AddDynamic(this, &UVGMissionInfoWidget::OnRep_MissionStateChanged);
+}
+
+void UVGMissionInfoWidget::OnRep_MissionStateChanged(int32 MissionID, FGameplayTag NewStateTag)
+{
+	FLinearColor TextColor;
+	if (NewStateTag == VigilantMissionTags::MissionCompleted)
+	{
+		TextColor = FLinearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	}
+	else if (NewStateTag == VigilantMissionTags::MissionActive)
+	{
+		TextColor = FLinearColor(0.8f, 0.8f, 0.2f, 1.0f);
+	}
+	else
+	{
+		TextColor = FLinearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	}
+	MissionDescriptionText->SetColorAndOpacity(FSlateColor(TextColor));
+}
---
+++ Source/Vigilant/UI/VGMissionInfoWidget.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameplayTags.h"
+#include "VGMissionInfoWidget.generated.h"
+
+class UTextBlock;
+class AVGMissionBase;
+
+UCLASS()
+class VIGILANT_API UVGMissionInfoWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	void RegisterMission(AVGMissionBase* Mission);
+	
+	void OnRep_MissionStateChanged(int32 MissionID, FGameplayTag NewStateTag);
+	
+protected:
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* MissionDescriptionText;
+};
