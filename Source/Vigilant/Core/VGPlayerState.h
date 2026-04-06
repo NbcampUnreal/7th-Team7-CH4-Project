b7cabdbddcb4534d219dc3ac6467fbe0a2ffc8ff
YongHo <skymo6872@gmail.com>
2026-04-06 10:42:01 +0900

    [이용호] PlayerState 태그 관련 로직 추가
    태그추가, 제거, 확인용 헬퍼함수 생성
    입장 후 모든 플레이어가 레디를 눌러야 게임이 시작되는 로직 추가

--- Source/Vigilant/Core/VGPlayerState.h
+++ Source/Vigilant/Core/VGPlayerState.h
@@ -13,7 +13,25 @@ class VIGILANT_API AVGPlayerState : public APlayerState
 	
 public:
 	
+	AVGPlayerState();
+	
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Vigilant|Tags")
 	FGameplayTagContainer PlayerTags;
+	// 태그 변경 헬퍼 함수(더하기)
+	UFUNCTION(BlueprintCallable, Category = "Vigilant|Tags")
+	void AddPlayerTag(const FGameplayTag& TagToAdd);
+	// 태그 변경 헬퍼 함수(빼기)
+	UFUNCTION(BlueprintCallable, Category = "Vigilant|Tags")
+	void RemovePlayerTag(const FGameplayTag& TagToRemove);
+	// 태그 변경 헬퍼 함수(매칭하기)
+	UFUNCTION(BlueprintCallable, Category = "Vigilant|Tags")
+	bool HasPlayerTag(const FGameplayTag& TagToCheck) const;
 	
+	UPROPERTY(Replicated,BlueprintReadOnly,Category = "Vigilant|Lobby")
+	bool bIsReady = false;
+	
+	UFUNCTION(Client, Reliable, Category = "Vigilant|Lobby")
+	void Client_ReceiveRole(FGameplayTag AssignedRoleTag);
+	
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 };
