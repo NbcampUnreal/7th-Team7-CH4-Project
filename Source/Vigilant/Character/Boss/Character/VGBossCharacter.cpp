b152eb348260ba02c23e70f62e9a1c30f7744b69
tkd6966 <tkd6966@gmail.com>
2026-04-07 21:04:17 +0900

    [하상빈] 보스 캐릭터 스킬 시스템(Q, E) 및 네트워크 동기화 구현
    - VGBossSkillComponent 추가 및 스킬(Q, E) 실행 뼈대 구현
    - 보스 전용 게임플레이 태그(VigilantBoss) 추가 (상태 제어 및 쿨타임)
    - RPC 애니메이션 동기화 적용
    - Enhanced Input 시스템을 활용한 스킬 단축키 바인딩
    - 보스 빙의 시 기존 이동 조작(WASD) 위에 스킬 조작(IMC_BossCombat)을 레이어로 추가

---
+++ Source/Vigilant/Character/Boss/Character/VGBossCharacter.cpp
@@ -0,0 +1,109 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VGBossCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Character//Boss/Component/VGBossSkillComponent.h"
+#include "Data/VGBossDataAsset.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Net/UnrealNetwork.h"
+
+
+AVGBossCharacter::AVGBossCharacter()
+{
+	// 보스의 덩치를 1.5배 크게 설정 (임시)
+	// SetActorScale3D(FVector(1.5f, 1.5f, 1.5f));
+
+	bReplicates = true;
+	JumpMaxCount = 0;
+	// 스킬 컴포넌트 생성 및 부착
+	SkillComponent = CreateDefaultSubobject<UVGBossSkillComponent>(TEXT("SkillComponent"));
+}
+
+void AVGBossCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// 게임이 시작될 때, 데이터 에셋이 연결되어 있다면 변수에 값 세팅
+	if (BossData != nullptr)
+	{
+		CurrentHealth = BossData->BaseHealth;
+		
+		if (GetCharacterMovement())
+		{
+			GetCharacterMovement()->MaxWalkSpeed = BossData->BossNormalSpeed;
+		}
+	}
+}
+
+void AVGBossCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+	if (EnhancedInputComponent)
+	{
+		// Q 버튼을 누르면 Input_SkillQ 실행
+		if (SkillAction_Q)
+		{
+			EnhancedInputComponent->BindAction(SkillAction_Q, ETriggerEvent::Triggered, this, &AVGBossCharacter::Input_SkillQ);
+		}
+        
+		// E 버튼을 누르면 Input_SkillE 실행
+		if (SkillAction_E)
+		{
+			EnhancedInputComponent->BindAction(SkillAction_E, ETriggerEvent::Triggered, this, &AVGBossCharacter::Input_SkillE);
+		}
+	}
+}
+
+void AVGBossCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+	
+	APlayerController* PlayerController = Cast<APlayerController>(NewController);
+	if (PlayerController)
+	{
+		UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+		if (Subsystem && BossMappingContext)
+		{
+			// 기존 조작법을 밀어내고 보스 조작법을 최우선(Priority 1)으로 덮어씌웁니다.
+			Subsystem->AddMappingContext(BossMappingContext, 1);
+		}
+	}
+}
+
+void AVGBossCharacter::Input_SkillQ(const FInputActionValue& Value)
+{
+	if (SkillComponent)
+	{
+		SkillComponent->ExecuteSkill_Q();
+	}
+}
+
+void AVGBossCharacter::Input_SkillE(const FInputActionValue& Value)
+{
+	if (SkillComponent)
+	{
+		SkillComponent->ExecuteSkill_E();
+	}
+}
+
+void AVGBossCharacter::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(AVGBossCharacter, CurrentHealth);
+}
+
+void AVGBossCharacter::InitializeBossStats(float InCalculatedHealth, float InCalculatedDamage)
+{
+	// 스탯 적용은 서버에서만 권한을 가짐
+	if (HasAuthority())
+	{
+		CurrentHealth = InCalculatedHealth;
+		
+		UE_LOG(LogTemp, Log, TEXT("보스 스탯 - 체력: %f, 공격력: %f"), CurrentHealth, InCalculatedDamage);
+	}
+}
--- Source/Vigilant/Common/VGGameplayTags.cpp
+++ Source/Vigilant/Common/VGGameplayTags.cpp
@@ -10,6 +10,10 @@ UE_DEFINE_GAMEPLAY_TAG(VigilantCharacter::Invincible, "Character.State.Invincibl
 UE_DEFINE_GAMEPLAY_TAG(VigilantCharacter::OccupiedLeftHand, "Character.State.Occupied.Left");
 UE_DEFINE_GAMEPLAY_TAG(VigilantCharacter::OccupiedRightHand, "Character.State.Occupied.Right");
 
+// 보스 태그 정의
+UE_DEFINE_GAMEPLAY_TAG(VigilantBoss::Casting, "Boss.State.Casting");
+UE_DEFINE_GAMEPLAY_TAG(VigilantBoss::SkillCooldown_Q, "Boss.Skill.Cooldown.Q");
+UE_DEFINE_GAMEPLAY_TAG(VigilantBoss::SkillCooldown_E, "Boss.Skill.Cooldown.E");
 
 //UE_DEFINE_GAMEPLAY_TAG(VigilantTags::Sprint, "Character.Action.Movable.Sprint");
 UE_DEFINE_GAMEPLAY_TAG(VigilantMissionTags::MissionInactive, "Mission.State.Inactive");
