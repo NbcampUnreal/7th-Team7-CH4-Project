6170bd6bbe1ccf0a31309f236c8ede1ce6919706
정찬호_JCH <elvpfhhrm@gmail.com>
2026-04-07 17:05:12 +0900

    [정찬호] Sandbag을 쓰러뜨리는 미션 구현 중

---
+++ Source/Vigilant/Mission/Definitions/VGMissionCombat.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VGMissionBase.h"
+#include "VGMissionCombat.generated.h"
+
+class AVGMissionSandbag;
+class AVGCharacterBase;
+
+UCLASS()
+class VIGILANT_API AVGMissionCombat : public AVGMissionBase
+{
+	GENERATED_BODY()
+
+public:
+	AVGMissionCombat();
+
+protected:
+	virtual void BeginPlay() override;
+	
+private:
+	UFUNCTION()
+	void OnSandbagDefeated(AVGCharacterBase* LastAttacker);
+
+	void GiveRewardToPlayer(AVGCharacterBase* Player);
+	bool AreAllSandbagsDefeated() const;
+
+public:
+	// 에디터에서 이 미션에 사용할 샌드백 등록
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Mission|Combat")
+	TArray<TObjectPtr<AVGMissionSandbag>> MissionSandbags;
+};
