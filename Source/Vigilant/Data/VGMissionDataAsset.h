af8bae595630b48270186175a62193b0fc062de7
정찬호_JCH <elvpfhhrm@gmail.com>
2026-04-08 18:48:57 +0900

    [정찬호] Mission 정보를 담을 DataAsset 생성

---
+++ Source/Vigilant/Data/VGMissionDataAsset.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Common/VGGameplayTags.h"
+#include "VGMissionDataAsset.generated.h"
+
+UCLASS()
+class VIGILANT_API UVGMissionDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Mission")
+	int32 MissionID = -1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Mission")
+	FString MissionDescription;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Mission")
+	FGameplayTag MissionTypeTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Mission|Reward")
+	TSubclassOf<AVGEquippableActor> RewardItemClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Mission|TimeAttack")
+	float TimeLimit = 30.f;
+};
