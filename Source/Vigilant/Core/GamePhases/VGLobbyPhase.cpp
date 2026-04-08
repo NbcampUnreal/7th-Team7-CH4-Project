cdb7c0d9130678ea9055a0b78317d8f739b4339b
YongHo <skymo6872@gmail.com>
2026-04-08 15:55:34 +0900

    [이용호] 로비 페이즈 규칙 생성
    로비페이즈에서 사용할 규칙 및 로직 작성

--- Source/Vigilant/Core/GamePhases/VGLobbyPhase.cpp
+++ Source/Vigilant/Core/GamePhases/VGLobbyPhase.cpp
@@ -1,4 +1,39 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "VGLobbyPhase.h"
+#include "Core/VGGameMode.h"
+#include "Common/VGGameplayTags.h"
+
+void UVGLobbyPhase::EnterPhase()
+{
+	Super::EnterPhase();
+	
+	UE_LOG(LogTemp, Warning, TEXT("[VGLobbyPhase] 로비 페이즈 진입"));
+}
+
+void UVGLobbyPhase::ExitPhase()
+{
+	UE_LOG(LogTemp, Warning, TEXT("[VGLobbyPhase] 전원 레디 완료! 로비 페이즈 종료"));
+	Super::ExitPhase();
+}
+
+void UVGLobbyPhase::ExecutePhaseResult()
+{
+	if (GameModeRef && NextPhaseClass)
+	{
+		GameModeRef->TransitionToPhase(NextPhaseClass);
+	}
+}
+
+bool UVGLobbyPhase::CanPlayerAttack(AVGCharacterBase* Attacker, AVGCharacterBase* Target)
+{
+	return false;
+}
+
+bool UVGLobbyPhase::CanPlayerTakeDamage(AActor* DamageCauser, AVGCharacterBase* Target)
+{
+	return false;
+}
+
+bool UVGLobbyPhase::CanPlayerInteract(AVGCharacterBase* Player, AActor* InteractableObject)
+{
+	return false;
+}
