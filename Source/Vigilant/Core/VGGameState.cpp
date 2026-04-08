1f3632723d4b081619563f81aa230869e6890209
YongHo <skymo6872@gmail.com>
2026-04-08 15:52:40 +0900

    [이용호] GameState 작성
    UI에서 사용할 수 있도록 현재 게임 상태와 태그 GameState에 추가

--- Source/Vigilant/Core/VGGameState.cpp
+++ Source/Vigilant/Core/VGGameState.cpp
@@ -1,5 +1,57 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Core/VGGameState.h"
+#include "Net/UnrealNetwork.h"
 
+AVGGameState::AVGGameState()
+{
+	// 초기값 세팅
+	CurrentPhaseTag = FGameplayTag::EmptyTag;
+	TotalMissionProgress = 0.0f;
+	LastDuelWinnerName = TEXT("");
+	PhaseEndTime = 0.0f;
+	BossNerfRate = 1.0f;
+}
+
+float AVGGameState::GetRemainingPhaseTime() const
+{
+	if (PhaseEndTime < 0.0f)
+	{
+		return -1.0f;
+	}
+	float TimeLeft = PhaseEndTime - GetServerWorldTimeSeconds();
+	
+	return FMath::Max(0.0f, TimeLeft);
+}
+
+void AVGGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(AVGGameState, CurrentPhaseTag);
+	DOREPLIFETIME(AVGGameState, TotalMissionProgress);
+	DOREPLIFETIME(AVGGameState, LastDuelWinnerName);
+	DOREPLIFETIME(AVGGameState, PhaseEndTime);
+	DOREPLIFETIME(AVGGameState, BossNerfRate);
+}
+
+void AVGGameState::OnRep_CurrentPhaseTag()
+{
+	OnPhaseChanged.Broadcast(CurrentPhaseTag);
+}
+
+void AVGGameState::OnRep_MissionProgress()
+{
+	OnMissionProgressUpdated.Broadcast(TotalMissionProgress);
+}
+
+void AVGGameState::OnRep_DuelWinner()
+{
+	if (!LastDuelWinnerName.IsEmpty())
+	{
+		OnDuelWinnerAnnounced.Broadcast(LastDuelWinnerName);
+	}
+}
+
+void AVGGameState::OnRep_BossNerfRate()
+{
+	OnBossNerfUpdated.Broadcast(BossNerfRate);
+}
