f09388a4386a08e66f933f381ecccc754ca6caa0
YongHo <skymo6872@gmail.com>
2026-04-09 16:49:27 +0900

    [이용호] UI와 투표시스템 연동
    투표 버튼 입력을 받았을 때 UI와 PlayerController 간 로직 연동

--- Source/Vigilant/Subsystem/VGUIManagerSubsystem.cpp
+++ Source/Vigilant/Subsystem/VGUIManagerSubsystem.cpp
@@ -6,6 +6,7 @@
 #include "Blueprint/UserWidget.h"
 #include "Data/VGUIDataAsset.h"
 #include "Core/DeveloperSettings/VGDevelopSettings.h"
+#include "Core/VGPlayerController.h"
 #include "UI/VGHUDWidget.h"
 #include "UI/VGPopupWidget.h"
 #include "UI/VGVoteWidget.h"
@@ -34,6 +35,19 @@ void UVGUIManagerSubsystem::OnHealthUpdate(float NewValue, float MaxValue)
 	if (CurrentHUDWidget)
 	{
 		CurrentHUDWidget->UpdateHealthUI(NewValue, MaxValue);
+	}
+}
+
+void UVGUIManagerSubsystem::RequestSubmitVote(int32 TargetIndex)
+{
+	if (ULocalPlayer* LocalPlayer = GetLocalPlayer())
+	{
+		APlayerController* BasePlayerController = LocalPlayer->GetPlayerController(GetWorld());
+		if (AVGPlayerController* VGPlayerController = Cast<AVGPlayerController>(BasePlayerController))
+		{
+			VGPlayerController->Server_SubmitVote(TargetIndex);
+			UE_LOG(LogTemp, Log, TEXT("[VGUIManagerSubsystem] %d번 슬롯 투표 성공"), TargetIndex);
+		}
 	}
 }
 
