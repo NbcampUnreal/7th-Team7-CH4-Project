134dae9b78931cd436d075b8a08d4b058fb11b0a
kbrother102 <kbrother102@gmail.com>
2026-04-09 18:51:42 +0900

    [김형백] 타이틍 화면 추가

---
+++ Source/Vigilant/UI/UVGTitleWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UVGTitleWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIGILANT_API UUVGTitleWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
