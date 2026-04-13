d118cee54889ecb7959a9500f120717223579c75
YongHo <skymo6872@gmail.com>
2026-04-13 18:43:02 +0900

    [이용호] 듀얼페이즈 스폰 및 퇴장 로직 추가
    막고라페이즈에 진입했을 때 플레이어를 스폰시키고 끝났을 때 원래위치로 복귀시키는 로직 추가

--- Source/Vigilant/Core/GamePhases/VGDuelPhase.cpp
+++ Source/Vigilant/Core/GamePhases/VGDuelPhase.cpp
@@ -3,6 +3,7 @@
 #include "Character/VGCharacterBase.h"
 #include "Core/VGPlayerState.h"
 #include "Common/VGGameplayTags.h"
+#include "GameFramework/PlayerStart.h"
 #include "Kismet/GameplayStatics.h"
 
 void UVGDuelPhase::EnterPhase()
@@ -14,52 +15,114 @@ void UVGDuelPhase::EnterPhase()
 	AVGCharacterBase* Player1 = GameModeRef->GetDuelChallenger();
 	AVGCharacterBase* Player2 = GameModeRef->GetDuelTarget();
 	
-	if (Player1 && Player2)
+	if (!Player1 || !Player2) return;
+	
+	// 아레나 스폰 포인트 검색 및 분류
+	TArray<AActor*> AllStarts;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerStart::StaticClass(), AllStarts);
+	
+	ParticipantStarts.Empty();
+	SpectatorStarts.Empty();
+	
+	for (AActor* Start : AllStarts)
 	{
-		// 전체 플레이어 순회
-		for (APlayerState* PlayerState : GameModeRef->GetWorld()->GetGameState()->PlayerArray)
+		if (APlayerStart* PlayerStart = Cast<APlayerStart>(Start))
 		{
-			AVGPlayerState* VGPlayerState = Cast<AVGPlayerState>(PlayerState);
-			if (!VGPlayerState) continue;
-
-			AVGCharacterBase* Character = Cast<AVGCharacterBase>(VGPlayerState->GetPawn());
-			if (!Character) continue;
-
-			// 대상이 참가자인 경우
-			if (Character == Player1 || Character == Player2)
+			if (PlayerStart->PlayerStartTag == FName("DuelParticipant"))
 			{
-				// 참가자 태그 부여
-				VGPlayerState->AddPlayerTag(VigilantStateTags::DuelParticipant);
-				
-				// 참가자를 막고라장으로 텔레포트하는 로직 추가 예정
-				
+				ParticipantStarts.Add(PlayerStart);
 			}
-			// 대상이 참가자가 아니라면
-			else
+			else if (PlayerStart->PlayerStartTag == FName("DuelSpectator"))
 			{
-				// 관객 태그 부여
-				VGPlayerState->AddPlayerTag(VigilantStateTags::DuelSpectator);
-
-				// 관객 텔레포르 로직 추가 예정
-				
+				SpectatorStarts.Add(PlayerStart);
 			}
-			
+		}
+	}
+	
+	int32 ParticipantIndex = 0;
+	int32 SpectatorIndex = 0;
+	
+	// 전체 플레이어 순회(태그 부여, 텔레포트 진행)
+	for (APlayerState* PlayerState : GameModeRef->GetWorld()->GetGameState()->PlayerArray)
+	{
+		AVGPlayerState* VGPlayerState = Cast<AVGPlayerState>(PlayerState);
+		if (!VGPlayerState) continue;
+
+		AVGCharacterBase* VGCharacter = Cast<AVGCharacterBase>(VGPlayerState->GetPawn());
+		if (!VGCharacter) continue;
+
+		// 막고라장으로 옮기기전 플레이어들 Transform 정보 저장
+		OriginalTransforms.Add(VGPlayerState, VGCharacter->GetActorTransform());
+
+		AActor* TargetStart = nullptr;
+
+		// 참가자일 때 태그 부여 및 스폰포인트 지정
+		if (VGCharacter == Player1 || VGCharacter == Player2)
+		{
+			VGPlayerState->AddPlayerTag(VigilantStateTags::DuelParticipant);
+			TargetStart = ParticipantStarts.IsValidIndex(ParticipantIndex) ? ParticipantStarts[ParticipantIndex++] : nullptr;
+		}
+		// 참가자 아닐 때 태그 부여 및 스폰포인트 지정
+		else
+		{
+			VGPlayerState->AddPlayerTag(VigilantStateTags::DuelSpectator);
+			TargetStart = SpectatorStarts.IsValidIndex(SpectatorIndex) ? SpectatorStarts[SpectatorIndex++] : nullptr;
 		}
 
-		UE_LOG(LogTemp, Warning, TEXT("[VGDuelPhase] 태그 부여 완료. 막고라 페이즈 시작"));
-	
+		// 배정된 PlayerStart로 이동
+		if (TargetStart)
+		{
+			FVector Loc = TargetStart->GetActorLocation();
+			FRotator Rot = TargetStart->GetActorRotation();
+
+			if (APlayerController* PC = Cast<APlayerController>(VGCharacter->GetController()))
+			{
+				PC->SetControlRotation(Rot);
+				PC->ClientSetRotation(Rot);
+			}
+			VGCharacter->TeleportTo(Loc, Rot, false, true);
+		}
 	}
+	
+
 }
 
 void UVGDuelPhase::ExitPhase()
 {
-	// 플레이어들 막고라 관련 태그 해제
-	for (APlayerState* PS : GameModeRef->GetWorld()->GetGameState()->PlayerArray)
+	// 플레이어 원위치 복귀
+	for (auto& Pair : OriginalTransforms)
 	{
-		if (AVGPlayerState* VGPS = Cast<AVGPlayerState>(PS))
+		AVGPlayerState* VGPlayerState = Pair.Key;
+		FTransform OriginalTarget = Pair.Value;
+
+		if (VGPlayerState && VGPlayerState->GetPawn())
 		{
-			VGPS->RemovePlayerTag(VigilantStateTags::DuelParticipant);
-			VGPS->RemovePlayerTag(VigilantStateTags::DuelSpectator);
+			APawn* Pawn = VGPlayerState->GetPawn();
+			FVector Loc = OriginalTarget.GetLocation();
+			FRotator Rot = OriginalTarget.GetRotation().Rotator();
+
+			// 시점 동기화
+			if (APlayerController* PlayerController = Cast<APlayerController>(Pawn->GetController()))
+			{
+				PlayerController->SetControlRotation(Rot);
+				PlayerController->ClientSetRotation(Rot);
+			}
+			Pawn->TeleportTo(Loc, Rot, false, true);
+		}
+	}
+	
+	// 메모리 정리
+	OriginalTransforms.Empty();
+	ParticipantStarts.Empty();
+	SpectatorStarts.Empty();
+	
+	// 플레이어들 막고라 관련 태그 해제
+	for (APlayerState* PlayerState : GameModeRef->GetWorld()->GetGameState()->PlayerArray)
+	{
+		if (AVGPlayerState* VGPlayerState = Cast<AVGPlayerState>(PlayerState))
+		{
+			VGPlayerState->RemovePlayerTag(VigilantStateTags::DuelParticipant);
+			VGPlayerState->RemovePlayerTag(VigilantStateTags::DuelSpectator);
 		}
 	}
 	
