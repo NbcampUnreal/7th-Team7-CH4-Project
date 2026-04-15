3f90db89f9129e207c47c759e9c7d05ce0677e58
정찬호_JCH <elvpfhhrm@gmail.com>
2026-04-15 17:38:07 +0900

    [정찬호] Altar에 힌트 이펙트 추가 시도 중

--- Source/Vigilant/Mission/Gimmick/VGMissionGimmickAltar.cpp
+++ Source/Vigilant/Mission/Gimmick/VGMissionGimmickAltar.cpp
@@ -1,10 +1,15 @@
 ﻿#include "VGMissionGimmickAltar.h"
+
+#include "NiagaraComponentPool.h"
 #include "Common/VGGameplayTags.h"
 #include "Character/VGCharacterBase.h"
 #include "Character/Component/VGEquipmentComponent.h"
 #include "Mission/Item/VGMissionItemCarry.h"
 #include "Mission/Item/VGMissionItemBase.h"
 #include "Data/VGMissionItemDataAsset.h"
+#include "NiagaraSystem.h"
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
 
 AVGMissionGimmickAltar::AVGMissionGimmickAltar()
 {
@@ -42,7 +47,13 @@ bool AVGMissionGimmickAltar::CanInteractWith(AActor* Interactor) const
 			continue;
 		}
 		
-		if (FindMissionItemByTag(EquipComp, Slot.RequiredItemTypeTag))
+		UVGMissionItemDataAsset* ItemDataAsset = Slot.ItemDataAsset.Get();
+		if (!ItemDataAsset)
+		{
+			continue;
+		}
+		
+		if (FindMissionItemByTag(EquipComp, ItemDataAsset->ItemTypeTag))
 		{
 			return true;
 		}
@@ -88,15 +99,102 @@ void AVGMissionGimmickAltar::OnInteractWith(AActor* Interactor, const FTransform
 	// 모든 슬롯이 채워졌으면 완료
 	if (AreAllSlotsFilled())
 	{
+		GetWorldTimerManager().ClearTimer(HintVisibilityTimerHandle);
 		SetStateTag(VigilantMissionTags::GimmickCompleted);
+	}
+}
+
+void AVGMissionGimmickAltar::UpdateHintEffectVisibility()
+{
+	APlayerController* LocalPC = GetWorld()->GetFirstPlayerController();
+	if (!LocalPC || !LocalPC->GetPawn())
+	{
+		return;
+	}
+	
+	float DistSq = FVector::DistSquared(
+		GetActorLocation(),
+		LocalPC->GetPawn()->GetActorLocation()
+	);
+    
+	bool bShouldShow = DistSq < FMath::Square(HintVisibleRange);
+    
+	for (int32 i = 0; i < HintEffectComponents.Num(); i++)
+	{
+		UNiagaraComponent* Comp = HintEffectComponents[i];
+		if (!Comp)
+		{
+			continue;
+		}
+		
+		// 이미 채워진 슬롯은 무조건 비활성
+		bool bSlotEmpty = !PlacementSlots[i].IsOccupied();
+		bool bActivate = bShouldShow && bSlotEmpty;
+        
+		if (bActivate && !Comp->IsActive())
+		{
+			Comp->Activate();
+		}
+		else if (!bActivate && Comp->IsActive())
+		{
+			Comp->Deactivate();
+		}
+	}
+}
+
+void AVGMissionGimmickAltar::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	for (int32 i = 0; i < PlacementSlots.Num(); i++)
+	{
+		const FVGAltarPlacementSlot& Slot = PlacementSlots[i];
+        
+		if (!Slot.RequiredItemHintEffect) continue;
+        
+		UNiagaraComponent* NiagaraComp = NewObject<UNiagaraComponent>(this);
+		NiagaraComp->SetAsset(Slot.RequiredItemHintEffect);
+		NiagaraComp->SetupAttachment(RootComponent);
+		NiagaraComp->SetRelativeLocation(Slot.AttachOffset);
+		NiagaraComp->RegisterComponent();
+        
+		// 메쉬 정보 전달 (Niagara에 User.Mesh 파라미터가 있을 경우)
+		UVGMissionItemDataAsset* ItemDataAsset = Slot.ItemDataAsset.Get();
+		if (!ItemDataAsset)
+		{
+			continue;
+		}
+		
+		NiagaraComp->SetVariableStaticMesh(FName("User.Mesh"), ItemDataAsset->ItemMesh);
+        
+		HintEffectComponents.Add(NiagaraComp);
+	}
+	
+	// Player와의 거리 체크
+	APlayerController* LocalPC = GetWorld()->GetFirstPlayerController();
+	if (LocalPC && LocalPC->IsLocalController())
+	{
+		GetWorldTimerManager().SetTimer(
+			HintVisibilityTimerHandle,
+			this,
+			&AVGMissionGimmickAltar::UpdateHintEffectVisibility,
+			0.3f,
+			true
+		);
 	}
 }
 
 bool AVGMissionGimmickAltar::TryPlaceItemToSlot(UVGEquipmentComponent* EquipComp, FVGAltarPlacementSlot& Slot)
 {
 	// 헬퍼로 일치하는 아이템을 찾은 뒤 CarryItem으로 캐스팅
+	UVGMissionItemDataAsset* ItemDataAsset = Slot.ItemDataAsset.Get();
+	if (!ItemDataAsset)
+	{
+		return false;
+	}
+	
 	AVGMissionItemBase* FoundItem =
-		FindMissionItemByTag(EquipComp, Slot.RequiredItemTypeTag);
+		FindMissionItemByTag(EquipComp, ItemDataAsset->ItemTypeTag);
 	
 	AVGMissionItemCarry* CarryItem = Cast<AVGMissionItemCarry>(FoundItem);
 	if (!CarryItem)
@@ -128,6 +226,7 @@ bool AVGMissionGimmickAltar::AreAllSlotsFilled() const
 			return false;
 		}
 	}
+	
 	return true;
 }
 
