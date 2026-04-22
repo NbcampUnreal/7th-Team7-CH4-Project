b441f6548ab861fad6a7ec008e2d134d2867fda8
YongHo <skymo6872@gmail.com>
2026-04-22 19:27:34 +0900

    [이용호] 듀얼페이즈 승리조건 수정
    듀얼 페이즈에서 참가자 중 한 명의 체력이 20%미만이 되면 끝나도록 수정

--- Source/Vigilant/Core/GamePhases/VGDuelPhase.h
+++ Source/Vigilant/Core/GamePhases/VGDuelPhase.h
@@ -34,4 +34,14 @@ protected:
 	UPROPERTY(Transient)
 	TArray<AActor*> SpectatorStarts;
 	
+	bool bIsDuelFinished = false;
+
+	// 각 참가자의 체력 변화를 감지할 델리게이트 함수
+	UFUNCTION()
+	void OnChallengerHPChanged(float NewHP, float MaxHP);
+	UFUNCTION()
+	void OnTargetHPChanged(float NewHP, float MaxHP);
+
+	// 20% 미만이 되었을 때 호출할 공통 패배 처리 함수
+	void HandleDuelDefeat(AVGCharacterBase* DefeatedPlayer);
 };
